90a40808a0830fe5c767658f1e8c33c218825d26
Mikel <mikelgmh@gmail.com>
2020-12-03 09:31:58 +0100

    Product collection

--- src/java/myapplication/entity/User.java
+++ src/java/myapplication/entity/User.java
@@ -7,12 +7,15 @@ package myapplication.entity;
 
 import java.io.Serializable;
 import java.security.Timestamp;
+import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
@@ -50,6 +53,9 @@ public class User implements Serializable {
     @ManyToOne()
     @JoinColumn(name = "company_id")
     private Company company;
+    
+     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Product> products;
 
     public String getUsername() {
         return username;
