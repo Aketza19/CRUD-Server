bfee6266d4dd0bb4a2a3dc3cc2dd75c634420b42
Mikel <mikelgmh@gmail.com>
2020-12-21 09:51:54 +0100

    .gitignore modificado para ignorar los archivos de propiedades, archivo de propiedades de ejemplo añadido, creada una clase nueva con métodos de cifrado por clave privada que se obtiene del archivo de propiedades

---
+++ src/java/myapplication/utils/security/SymmetricEncryption.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myapplication.utils.security;
+
+import java.security.spec.KeySpec;
+import java.util.Base64;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+
+/**
+ * @author Mikel
+ */
+public class SymmetricEncryption {
+
+    private static String salt = "!JDEIF%FERFT&$d346";
+    private static ResourceBundle rb = ResourceBundle.getBundle("config.config");
+    private static String secretKey = "";
+
+    public SymmetricEncryption() {
+        this.secretKey = rb.getString("SYMMETRIC_ENCRYPTION_KEY");
+    }
+
+    /**
+     * Encrypts a string using the secret key stored in the properties file.
+     * @param strToEncrypt The string to be encrypted.
+     * @return 
+     */
+    public static String encrypt(String strToEncrypt) {
+        try {
+            byte[] iv = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+            IvParameterSpec ivspec = new IvParameterSpec(iv);
+
+            SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+            KeySpec spec = new PBEKeySpec(secretKey.toCharArray(), salt.getBytes(), 65536, 256);
+            SecretKey tmp = factory.generateSecret(spec);
+            SecretKeySpec secretKey = new SecretKeySpec(tmp.getEncoded(), "AES");
+
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey, ivspec);
+            return Base64.getEncoder().encodeToString(cipher.doFinal(strToEncrypt.getBytes("UTF-8")));
+        } catch (Exception e) {
+            Logger.getLogger(Encryption.class.getName()).log(Level.SEVERE, null, e);
+        }
+        return null;
+    }
+
+    /**
+     * Decrypts a text using the secret.
+     * @param strToDecrypt The string to be decrypted.
+     * @return 
+     */
+    public static String decrypt(String strToDecrypt) {
+        try {
+            byte[] iv = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+            IvParameterSpec ivspec = new IvParameterSpec(iv);
+
+            SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+            KeySpec spec = new PBEKeySpec(secretKey.toCharArray(), salt.getBytes(), 65536, 256);
+            SecretKey tmp = factory.generateSecret(spec);
+            SecretKeySpec secretKey = new SecretKeySpec(tmp.getEncoded(), "AES");
+
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+            cipher.init(Cipher.DECRYPT_MODE, secretKey, ivspec);
+            return new String(cipher.doFinal(Base64.getDecoder().decode(strToDecrypt)));
+        } catch (Exception e) {
+            Logger.getLogger(Encryption.class.getName()).log(Level.SEVERE, null, e);
+        }
+        return null;
+    }
+}
