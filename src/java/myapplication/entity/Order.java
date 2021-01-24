8ca87083337b72593a7ea45b782d4de8c09406ce
Imanol Montoro <imanolma90@gmail.com>
2021-01-24 22:14:24 +0100

    Suprimir generado automático
    Error al insertar ordenes. Suprimir generado automático de ID

--- src/java/myapplication/entity/Order.java
+++ src/java/myapplication/entity/Order.java
@@ -41,7 +41,6 @@ public class Order implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
     
     @Temporal(TemporalType.DATE)
